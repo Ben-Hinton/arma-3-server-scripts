f0a2f753ce078bdbaf834459b69ccc126378d5e5
Ben Hinton <benhinton3@gmail.com>
2022-07-22 15:59:02 +0100

    Added comments to make code more readable

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ A script I made to simplify arma 3 server setup
 
 ## Usage:
 python3 script.py (link to arma mod profile) (user to log in with)
+
+If no user is provided then the script will log in as anonymously which may not work with all mods
--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import sys
 import os
 import shutil
 
-#functions that will be used later in the program
+#deletes all of the mods which have been downloaded previously
 def delete_mods():
     stuff = os.listdir()
     directory_list = []
@@ -11,9 +11,11 @@ def delete_mods():
         if(os.path.isdir(stuff[x]) and stuff[x][0] == '@'):
             shutil.rmtree(stuff[x])
 
+#updates the arma3 server install
 def update_server():
     os.system("steamcmd +force_install_dir /home/ben/Steam +login anonymous +app_update 233780 validate +exit")
 
+#removes all of the symlinks for mods so they dont load
 def remove_mods():
     stuff = os.listdir("/home/ben/Steam/steamapps/common/Arma 3 Server")
 
@@ -21,11 +23,13 @@ def remove_mods():
         if(stuff[x][0] == '@'):
             os.system("rm -rf /home/ben/Steam/steamapps/common/Arma\\ 3\\ Server/" + stuff[x])
 
+#downloads / updates all of the mods passed to it
 def run_steamcmd(links, user): 
     for x in range(0, len(links)):
         for i in range(0,5):
             os.system("steamcmd +force_install_dir /home/ben/Steam +login " + user + " +workshop_download_item 107410 " + links[x] + " validate +exit")
 
+#reads the arma3 mods file and extracts the workshop links from it
 def get_mods_from_file(file_name):
     f = open(file_name, mode='r')
     text = f.read()
@@ -45,9 +49,11 @@ def get_mods_from_file(file_name):
 
     return links
 
+#downloads the mod file passed to it
 def download_mod_file(link):
     os.system("wget " + link + " -O modfile.html")
 
+#generates a launch script to load the server with the required mods
 def generate_config_file(mods):
     text = "./arma3server_x64 -name=SPAC -config=server.cfg -mod="
     
@@ -59,35 +65,28 @@ def generate_config_file(mods):
     f.write(text)
     f.close()
 
-def move_mods():
-    stuff = os.listdir("/home/ben/Steam/steamapps/common/Arma 3 Server")
-    directory_list = []
-
-    for x in range (0, len(stuff)):
-        if(os.path.isdir(stuff[x])):
-            print(stuff[x])
-    
-    return directory_list
-
+#creates a simlink between the mods folder and the arma server folder
 def link_mods(links):
     for x in range(0, len(links)):
         os.system("ln -s /home/ben/Steam/steamapps/workshop/content/107410/" + links[x] + " /home/ben/Steam/steamapps/common/Arma\\ 3\\ Server/@" + links[x])
 
+#reanmes all the mod folders to lower case so the arma server is happy
 def rename_to_lower():
     os.system("find /home/ben/Steam/steamapps/workshop/content/107410/ -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;")
 
-#actual program starts here
+#if a mod file has not been provided then quit
 if len(sys.argv) < 2:
     quit()
 
 user = "anonymous"
 
+#if there are 3 arguments then a user has been provided, set the user to that argument
 if len(sys.argv) == 3:
     user = sys.argv[2]
 
-download_mod_file(sys.argv[1])
-
 update_server()
+
+download_mod_file(sys.argv[1])
 
 #delete_mods()
 
