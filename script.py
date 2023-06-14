9b4e4aab793e19dbbb44c5ae5d2e7a96158edf12
Ben Hinton <benhinton3@gmail.com>
2023-06-14 01:52:27 +0100

    Fixed the script to ensure mods load correctly

--- script.py
+++ script.py
@@ -2,7 +2,9 @@ import sys
 import os
 import shutil
 
-steamDirectory = "/home/ben/.steam/"
+steamDirectory = "/home/ben/Steam/"
+user = "anonymous"
+numberOfTimesToAttemptInstall = 3
 
 #deletes all of the mods which have been downloaded previously
 def delete_mods():
@@ -15,21 +17,21 @@ def delete_mods():
 
 #updates the arma3 server install
 def update_server():
-    os.system("steamcmd +login " + user + " +app_update 233780 validate +exit")
+    os.system("steamcmd +force_install_dir " + steamDirectory + "\"steamapps/common/Arma 3 Server\"" + " +login " + user + " +app_update 233780 validate +exit")
 
 #removes all of the symlinks for mods so they dont load
 def remove_mods():
-    stuff = os.listdir("/home/ben/.steam/steamapps/common/Arma 3 Server")
+    stuff = os.listdir(steamDirectory + "steamapps/common/Arma 3 Server")
 
     for x in range (0, len(stuff)):
         if(stuff[x][0] == '@'):
-            os.system("rm -rf /home/ben/.steam/steamapps/common/Arma\\ 3\\ Server/" + stuff[x])
+            os.system("rm -rf " + steamDirectory + "steamapps/common/Arma\\ 3\\ Server/" + stuff[x])
 
 #downloads / updates all of the mods passed to it
 def run_steamcmd(links, user): 
     for x in range(0, len(links)):
-        for i in range(0,4):
-            os.system("steamcmd +login " + user + " +workshop_download_item 107410 " + links[x] + " validate +exit")
+        for i in range(0,numberOfTimesToAttemptInstall):
+            os.system("steamcmd +force_install_dir " + steamDirectory + " +login " + user + " +workshop_download_item 107410 " + links[x] + " validate +exit")
 
 #reads the arma3 mods file and extracts the workshop links from it
 def get_mods_from_file(file_name):
@@ -57,30 +59,28 @@ def download_mod_file(link):
 
 #generates a launch script to load the server with the required mods
 def generate_config_file(mods):
-    text = "./arma3server_x64 -config=server.cfg"
+    text = "./arma3server_x64 -config=server.cfg -port=24001 -mod="
     
     text += "@" + mods[0]
     for x in range(1,len(mods)):
         text += "\\;@" + mods[x]
     
-    f = open("/home/ben/.steam/steamapps/common/Arma 3 Server/start.sh", mode='w')
+    f = open(steamDirectory + "steamapps/common/Arma 3 Server/start.sh", mode='w')
     f.write(text)
     f.close()
 
 #creates a simlink between the mods folder and the arma server folder
 def link_mods(links):
     for x in range(0, len(links)):
-        os.system("ln -s /home/ben/.steam/steamapps/workshop/content/107410/" + links[x] + " /home/ben/.steam/steamapps/common/Arma\\ 3\\ Server/@" + links[x])
+        os.system("ln -s " + steamDirectory + "steamapps/workshop/content/107410/" + links[x] + " " + steamDirectory + "steamapps/common/Arma\\ 3\\ Server/@" + links[x])
 
 #reanmes all the mod folders to lower case so the arma server is happy
 def rename_to_lower():
-    os.system("find /home/ben/.steam/steamapps/workshop/content/107410/ -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;")
+    os.system("find " + steamDirectory + "steamapps/workshop/content/107410/ -depth -exec rename 's/(.*)\/([^\/]*)/$1\/\L$2/' {} \;")
 
 #if a mod file has not been provided then quit
 if len(sys.argv) < 2:
     quit()
-
-user = "anonymous"
 
 #if there are 3 arguments then a user has been provided, set the user to that argument
 if len(sys.argv) == 3:
